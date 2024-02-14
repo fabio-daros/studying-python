911a4c752a83810ec884fd8898f1cc9fc9ff227c
Fabio Daros <43766793+fabio-daros@users.noreply.github.com>
2024-02-14 19:06:50 +0000

    Update ranges.py

--- ranges.py
+++ ranges.py
@@ -11,7 +11,7 @@ for num in range(11):
 for num2 in range(1, 11):
     print(num2)
 
-for num3 in range(1, 11, 2):  # De 1 a 10 de 2 em 2
+for num3 in range(1, 11, 2):  # 1 to 10 - 2 in 2
     print(num3)
 
 for num4 in range(10, 0, -1):
